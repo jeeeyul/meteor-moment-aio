ec9544aa41c6678463abc9ee7e41fea81d211130
Jeeeyul <Jeeeyul@gmail.com>
2014-02-13 09:43:18 +0900

    new moment

--- package.js
+++ package.js
@@ -9,7 +9,6 @@ Package.on_use(function(api, where) {
 		api.export("moment");
 	}
 
-	api.add_files('lib/moment-with-langs.min.js');
-	api.add_files('lib/korean-bug-fix.js');
+	api.add_files('lib/moment/min/moment-with-langs.min.js');
 	api.add_files('lib/export-moment.js');
 });
